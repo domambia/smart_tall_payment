d1f918b17418294cef8f308f83ca8ad2d4119ca9
macs-code <omambiadauglous@gmail.com>
2019-05-16 18:43:00 +0000

    [chore] adding the number plate

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired
 
 class PayForm(FlaskForm):
     phone 		 = StringField('Enter Phone (e.g254700000023)', validators=[DataRequired()])
+    number       = StringField("Vehicle Number Plater", validators=[DataRequired()])
     amount 		 = StringField('Enter Amount', validators=[DataRequired()])
 
     submit = SubmitField('Pay')
